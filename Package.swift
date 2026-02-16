5a3b5278fe2b847afd0a2774aa44909a16c5bdd5
Tarik ALAOUI M'HAMDI <71636934+tarikalaouimhamdi@users.noreply.github.com>
2026-02-16 23:41:06 +0000

    chore() - first commit 3.7.7 MobileVLCKit

--- Package.swift
+++ Package.swift
@@ -34,17 +34,17 @@ let package = Package(
     ),
     .binaryTarget(
       name: "MobileVLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.2/MobileVLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.7/MobileVLCKit.xcframework.zip",
       checksum: "5b21d834499fb3331f55cb60402e5814690665ac71b7bb24d033922283a5a364"
     ),
     .binaryTarget(
       name: "TVVLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.2/TVVLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.7/TVVLCKit.xcframework.zip",
       checksum: "013187589c0529793fe2f86ed267ddd237b30e6a5442f382bfbda05682f29305"
     ),
     .binaryTarget(
       name: "VLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.2/VLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/3.7.7/VLCKit.xcframework.zip",
       checksum: "f4aa1f88913952735f6aca013ade7cd61c4414a42b4d2c25056f82124452b4bb"
     ),
   ]
