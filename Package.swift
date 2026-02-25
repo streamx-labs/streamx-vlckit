411913056954c200582127ed9e625d6fc90adafb
Tarik ALAOUI M'HAMDI <71636934+tarikalaouimhamdi@users.noreply.github.com>
2026-02-25 01:27:06 +0000

    Update library names and URLs in Package.swift
    Updated library names and URLs for MobileVLCKit, TVVLCKit, and VLCKit to version 4.0.2.

--- Package.swift
+++ Package.swift
@@ -11,40 +11,32 @@ let package = Package(
   ],
   products: [
     .library(
-      name: "VLC",
-      targets: [
-        "VLC",
-      ]
+      name: "MobileVLCKit",
+      targets: ["MobileVLCKit"]
     ),
+    .library(
+      name: "TVVLCKit",
+      targets: ["TVVLCKit"]
+    ),
+    .library(
+      name: "VLCKit",
+      targets: ["VLCKit"]
+    )
   ],
   targets: [
-    .target(
-      name: "VLC",
-      dependencies: [
-        .target(name: "MobileVLCKit", condition: .when(platforms: [.iOS])),
-        .target(name: "TVVLCKit", condition: .when(platforms: [.tvOS])),
-        .target(name: "VLCKit", condition: .when(platforms: [.macOS])),
-      ],
-      path: "Sources",
-      linkerSettings: [
-        .linkedFramework("MobileVLCKit", .when(platforms: [.iOS])),
-        .linkedFramework("TVVLCKit", .when(platforms: [.tvOS])),
-        .linkedFramework("VLCKit", .when(platforms: [.macOS])),
-      ]
-    ),
     .binaryTarget(
       name: "MobileVLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.1/MobileVLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.2/MobileVLCKit.xcframework.zip",
       checksum: "5b21d834499fb3331f55cb60402e5814690665ac71b7bb24d033922283a5a364"
     ),
     .binaryTarget(
       name: "TVVLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.1/TVVLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.2/TVVLCKit.xcframework.zip",
       checksum: "013187589c0529793fe2f86ed267ddd237b30e6a5442f382bfbda05682f29305"
     ),
     .binaryTarget(
       name: "VLCKit",
-      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.1/VLCKit.xcframework.zip",
+      url: "https://github.com/streamx-labs/streamx-vlckit/releases/download/4.0.2/VLCKit.xcframework.zip",
       checksum: "f4aa1f88913952735f6aca013ade7cd61c4414a42b4d2c25056f82124452b4bb"
     ),
   ]
